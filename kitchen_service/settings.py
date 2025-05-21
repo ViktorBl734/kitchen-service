61d96b9ebc1141089674d6c4866dbf039e132c8b
ViktorBl734 <harbourprogress@gmail.com>
2025-05-21 12:45:46 +0300

    Move SECRET_KEY to .env file and update .gitignore

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -11,9 +11,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+if not SECRET_KEY:
+    raise Exception("DJANGO_SECRET_KEY environment variable is not set!")
 
 
 # Quick-start development settings - unsuitable for production
