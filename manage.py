eab7d89e065fc3ad2050cdcdd91bff4497aeeb6a
ViktorBl734 <harbourprogress@gmail.com>
2025-05-22 20:57:07 +0300

    Split settings into modules and configure PostgreSQL

--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
+load_dotenv()  # загружает переменные из .env
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'kitchen_service.settings.prod'))
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitchen_service.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
